06e879cf6846718108d818cbc859f35d1112bbaa
mao <1296193245@qq.com>
2024-01-14 14:26:58 +0800

    uniapp-device-clipboard

---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,49 @@
+<template>
+	<view>
+		<input style="background-color: antiquewhite;" placeholder="内容" type="text" v-model="value"/>
+		<button type="primary" @click="set">复制</button>
+		<button type="primary" @click="get">粘贴</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				value:''
+			}
+		},
+		methods: {
+			set()
+			{
+				uni.setClipboardData({
+					data:this.value,
+					success: () => {
+						console.log('成功');
+					},
+					fail: () => {
+						console.log('失败');
+					}
+				})
+			},
+			get()
+			{
+				uni.getClipboardData({
+					success: (data) => {
+						console.log('成功',data.data);
+						this.value=data.data;
+					},
+					fail: () => {
+						console.log('失败');
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 10px;
+}
+</style>
